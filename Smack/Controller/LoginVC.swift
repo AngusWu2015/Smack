fb6b38a36ff45b5e9b8fc00a1abfa38fbdc38e49
Andy.Wu <andy62210@hotmail.com>
2017-10-17 23:22:51 +0800

    new login UI

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by AndyWu on 2017/10/17.
+//  Copyright © 2017年 AndyWu. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ Smack/Utilitles/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by AndyWu on 2017/10/17.
+//  Copyright © 2017年 AndyWu. All rights reserved.
+//
+
+import Foundation
+
+let TO_LOFIN = "tologin"
