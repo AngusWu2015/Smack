9f93ca685a98159a385194eee83bc931e84a3d40
Andy.Wu <andy62210@hotmail.com>
2017-10-17 19:57:41 +0800

    新增切換目錄

---
+++ Smack/Controller/ChannelVC.swift
@@ -0,0 +1,18 @@
+//
+//  ChannelVC.swift
+//  Smack
+//
+//  Created by AndyWu on 2017/10/17.
+//  Copyright © 2017年 AndyWu. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+    }
+
+}
---
+++ Smack/Controller/ChatVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by AndyWu on 2017/10/17.
+//  Copyright © 2017年 AndyWu. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    @IBOutlet weak var menuBtn: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        
+        view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    }
+
+   
+
+}
+
