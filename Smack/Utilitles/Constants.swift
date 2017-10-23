319b423f6a1efd103ecff057c404d3e82c13c505
Andy.Wu <andy62210@hotmail.com>
2017-10-23 11:33:43 +0800

    add email API

--- Smack/Utilitles/Constants.swift
+++ Smack/Utilitles/Constants.swift
@@ -8,6 +8,20 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+// URL Constants
+let BASE_URL = "http://chatchatwu.herokuapp.com/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
+//Seguse
 let TO_LOFIN = "tologin"
 let TO_CREATEACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+
+//User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
