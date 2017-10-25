9dd5fa7029834c97c10919dd6991cdeeb5ece328
Andy.Wu <andy62210@hotmail.com>
2017-10-25 18:07:33 +0800

    new avatarPickerVC UI

--- Smack/Utilitles/Constants.swift
+++ Smack/Utilitles/Constants.swift
@@ -20,7 +20,7 @@ let URL_USER_ADD = "\(BASE_URL)user/add"
 let TO_LOFIN = "tologin"
 let TO_CREATEACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
-
+let TO_AVATAR_PICKER = "toAvatarPicker"
 
 //User Defaults
 let TOKEN_KEY = "token"
